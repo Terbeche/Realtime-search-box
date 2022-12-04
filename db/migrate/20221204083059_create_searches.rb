713d67a0b454edfabfb03f12c52119becc395ece
terbeche <termus96@gmail.com>
2022-12-04 09:32:29 +0100

    Fixed search model

--- db/migrate/20221204083059_create_searches.rb
+++ db/migrate/20221204083059_create_searches.rb
@@ -2,6 +2,7 @@ class CreateSearches < ActiveRecord::Migration[7.0]
   def change
     create_table :searches do |t|
       t.text :searching_for
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_080527) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_083059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,8 +23,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_080527) do
 
   create_table "searches", force: :cascade do |t|
     t.text "searching_for"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_080527) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "searches", "users"
 end
